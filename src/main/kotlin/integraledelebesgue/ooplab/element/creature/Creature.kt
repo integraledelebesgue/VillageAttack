2eb712d8541007a467c04f8a751afcabfd3b64c9
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-01-14 02:31:27 +0100

    Reflective properties and factories list/dictionaries

--- src/main/kotlin/integraledelebesgue/ooplab/element/creature/Creature.kt
+++ src/main/kotlin/integraledelebesgue/ooplab/element/creature/Creature.kt
@@ -1,11 +1,66 @@
 package integraledelebesgue.ooplab.element.creature
 
 import integraledelebesgue.ooplab.element.Vector2D
+import integraledelebesgue.ooplab.element.physicalobject.WallFactory
+import org.knowm.xchart.style.markers.Cross
 import java.util.UUID
+import kotlin.reflect.KClass
 
-sealed class Creature(var position: Vector2D, private val creatureProperties: CreatureProperties) {
+sealed class Creature(var position: Vector2D, val creatureProperties: CreatureProperties) {
+
+    var health = 0
+    var isAlive = true
 
     private val uuid: UUID = UUID.randomUUID()
+
+    fun takeDamage(damage: Int) {
+        health -= damage
+        if(health <= 0) isAlive = false
+    }
+
+    fun testCollisionWithWall(): Boolean {
+        return WallFactory.storage[position] != null
+    }
+
+    init {
+        health = creatureProperties.health
+    }
+
+    companion object {
+        val attackers: List<KClass<out Creature>> = listOf(Warewolf::class, Mummy::class, Zombie::class)
+        val defenders: List<KClass<out Creature>> = listOf(Archer::class, Crossbower::class, Mage::class)
+
+        val properties: Map<KClass<out Creature>, CreatureProperties> = hashMapOf(
+            Warewolf::class to WarewolfProperties,
+            Mummy::class to MummyProperties,
+            Zombie::class to ZombieProperties,
+            Archer::class to ArcherProperties,
+            Crossbower::class to CrossbowerProperties,
+            Mage::class to MageProperties
+        )
+
+        val factory: Map<KClass<out Creature>, CreatureFactory> = hashMapOf(
+            Warewolf::class to WarewolfFactory,
+            Mummy::class to MummyFactory,
+            Zombie::class to ZombieFactory,
+            Archer::class to ArcherFactory,
+            Crossbower::class to CrossbowerFactory,
+            Mage::class to MageFactory
+        )
+    }
+
+    class Warewolf(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
+
+    class Mummy(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
+
+    class Zombie(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
+
+    class Archer(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
+
+    class Crossbower(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
+
+    class Mage(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
+
 
     override fun hashCode(): Int {
         return uuid.hashCode()
@@ -21,21 +76,4 @@ sealed class Creature(var position: Vector2D, private val creatureProperties: Cr
 
         return true
     }
-
-    init {
-        var health = creatureProperties.health
-    }
-
-    class Warewolf(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
-
-    class Mummy(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
-
-    class Zombie(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
-
-    class Archer(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
-
-    class Crossbower(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
-
-    class Mage(position: Vector2D, creatureProperties: CreatureProperties): Creature(position, creatureProperties)
-
 }
